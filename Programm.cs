dc8562ba82687c389845eeaf74eedc326540e591
Neiki <67499407+NeikiDev@users.noreply.github.com>
2021-12-27 20:50:01 +0100

    Create Programm.cs

---
+++ Programm.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace hello_world
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
